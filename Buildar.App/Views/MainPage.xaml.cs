da3d328624b7f12dc114dee47d4764d2177a60a5
Stein Hanssen Sæstad <saestads@gmail.com>
2021-05-02 14:51:14 +0200

    Added MainPage.xaml/cs gridview item navigation to other pages.

--- Buildar.App/Views/MainPage.xaml.cs
+++ Buildar.App/Views/MainPage.xaml.cs
@@ -3,6 +3,7 @@
 using Buildar.App.ViewModels;
 
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 namespace Buildar.App.Views
 {
@@ -13,6 +14,34 @@ namespace Buildar.App.Views
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void GridViewContent_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            switch (e.ClickedItem.ToString())
+            {
+
+                case "Start a new build":
+                    this.Frame.Navigate(typeof(CustomBuildPage));
+                    break;
+
+                case "High End":
+                    this.Frame.Navigate(typeof(HighEndPage));
+                    break;
+
+                case "Mid Range":
+                    this.Frame.Navigate(typeof(MidRangePage));
+                    break;
+
+                case "Budget Build":
+                    this.Frame.Navigate(typeof(BudgetPage));
+                    break;
+
+                case "Community build":
+                    this.Frame.Navigate(typeof(MainPage));
+                    break;
+            }
+
         }
     }
 }
